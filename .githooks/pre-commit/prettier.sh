ce27dc3c306fe52c6f1fc71bf2e34d8aa6fc0282
Manuel Alabor <manuel@alabor.me>
2017-12-15 11:07:57 +0100

    Initial Commit

---
+++ .githooks/pre-commit/prettier.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E '(\.tsx|\.ts|\.js)$' | tr '\n' ' ')
+[ -z "$FILES" ] && exit 0
+
+# Prettify all staged files
+echo "$FILES" | xargs ./node_modules/.bin/prettier --write --single-quote --use-tabs --print-width 120
+
+# Add back the modified/prettified files to staging
+echo "$FILES" | xargs git add
+
+exit 0
