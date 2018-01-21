1772eb9ce23cc4faec849f022ea58b12eaea8a59
Manuel Alabor <manuel@alabor.me>
2018-01-21 12:28:23 +0100

    Switch to BehaviourSubject
    New subscribers did not get automatically the latest value emitted by
    the store. Using a BehaviourSubject instead of a plain Subject fixes
    this problem.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,35 @@ import { Connect } from './components/Connect';
 
 const store = createStore(); // Injectable via context?
 
-class App extends React.Component {
+class App extends React.Component<{}, { show: boolean }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { show: false };
+  }
+
   render() {
+    const { show } = this.state;
     return (
-      <Connect store={store}>
-        {(state, dispatch) => (
-          <div>
-            <button onClick={() => dispatch(Increment(1))}>Increment</button>
-            <div className="App">{state.count}</div>
-          </div>
+      <div>
+        <Connect store={store}>
+          {(state, dispatch) => (
+            <div>
+              <button onClick={() => dispatch(Increment(1))}>Increment</button>
+              <div className="App">{state.count}</div>
+            </div>
+          )}
+        </Connect>
+        <button onClick={() => this.setState(p => ({ show: !p.show }))}>Show</button>
+        {show && (
+          <Connect store={store}>
+            {state => (
+              <div>
+                <div className="App">{state.count}</div>
+              </div>
+            )}
+          </Connect>
         )}
-      </Connect>
+      </div>
     );
   }
 }
--- src/store/streams.ts
+++ src/store/streams.ts
@@ -8,11 +8,10 @@ export const defaultState: State = {
 const action$ = new Rx.Subject<ReducerFn>();
 
 export function createStore(initialState: State = defaultState): Store {
-  const store = new Rx.Subject<State>();
-  const store$ = store.startWith(initialState);
+  const store$ = new Rx.BehaviorSubject<State>(initialState);
 
   // Connect action$ stream with the store$ stream
-  action$.withLatestFrom(store$, (reduce, prev) => reduce(prev)).subscribe(store);
+  action$.withLatestFrom(store$, (reduce, prev) => reduce(prev)).subscribe(store$);
 
   return store$;
 }
